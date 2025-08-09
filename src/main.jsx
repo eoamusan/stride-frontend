c0daf67285d816d14fc8bf7098d03dc66199a850
Oluwatobi Amusan <amusantobi@gmail.com>
2025-08-10 00:18:49 +0100

    feat: add basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+// Import page components
+import LandingPage from './pages/landing/page.jsx';
+import LoginPage from './pages/auth/login/page.jsx';
+import RegisterPage from './pages/auth/register/page.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+]);
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
-    </StrictMode>,
+      <RouterProvider router={router} />
+    </StrictMode>
   );
 }
