72ed0bfaa8baf9e0a4723d72421b03d0d0f7812d
Oluwatobi Amusan <amusantobi@gmail.com>
2025-09-02 12:23:30 +0100

    feat: add invoice stats to invoicing page

--- src/pages/dashboard/accounting/invoicing/page.jsx
+++ src/pages/dashboard/accounting/invoicing/page.jsx
@@ -2,8 +2,33 @@ import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router';
 import EmptyInvoice from '@/components/dashboard/accounting/invoicing/empty-state';
 import CreateInvoice from '@/components/dashboard/accounting/invoicing/create-invoice';
+import { Button } from '@/components/ui/button';
+import { DownloadIcon, Key, PlusCircle, SettingsIcon } from 'lucide-react';
+import MetricCard from '@/components/dashboard/metric-card';
 
-const invoice = [];
+const invoice = [''];
+const invoiceStats = [
+  {
+    title: 'Total Invoices',
+    unit: '',
+  },
+  {
+    title: 'Total Amount',
+    unit: '$',
+  },
+  {
+    title: 'Outstanding Invoices',
+    unit: '$',
+  },
+  {
+    title: 'Unpaid Invoices',
+    unit: '',
+  },
+  {
+    title: 'Collection Rate',
+    unit: '%',
+  },
+];
 
 export default function Invoicing() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -44,16 +69,48 @@ export default function Invoicing() {
 
   return (
     <div className="my-4 min-h-screen">
-      <hgroup>
-        <h1 className="text-2xl font-bold">Invoice Management</h1>
-        <p className="text-sm text-[#7D7D7D]">
-          Manage your accounts receivable
-        </p>
-      </hgroup>
+      <div className="flex items-center justify-between">
+        <hgroup>
+          <h1 className="text-2xl font-bold">Invoice Management</h1>
+          <p className="text-sm text-[#7D7D7D]">
+            Manage your accounts receivable
+          </p>
+        </hgroup>
+
+        {invoice.length > 0 && (
+          <div className="flex space-x-4">
+            <Button
+              onClick={handleToggleCreateInvoice}
+              className={'h-10 rounded-2xl'}
+            >
+              <PlusCircle className="size-4" />
+              Create Invoice
+            </Button>
+            <Button size={'icon'} className={'size-10'} variant={'outline'}>
+              <DownloadIcon size={16} />
+            </Button>
+            <Button size={'icon'} className={'size-10'} variant={'outline'}>
+              <SettingsIcon size={16} />
+            </Button>
+          </div>
+        )}
+      </div>
       {invoice.length === 0 ? (
         <EmptyInvoice onClick={handleToggleCreateInvoice} />
       ) : (
-        <div>Display invoices here</div>
+        <div className="my-10 flex w-full flex-wrap gap-4">
+          {invoiceStats.length > 0 &&
+            invoiceStats.map((stat, index) => (
+              <MetricCard
+                key={index}
+                className="w-full max-w-[259px]"
+                title={stat.title}
+                unit={stat.unit}
+                emptyState={true}
+                {...stat}
+              />
+            ))}
+        </div>
       )}
     </div>
   );
