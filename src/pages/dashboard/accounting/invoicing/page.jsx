e9baaf20da16cb7fe1c564edc175afe6dd7cb92e
Oluwatobi Amusan <amusantobi@gmail.com>
2025-09-02 13:13:05 +0100

    feat: create invoice table.

--- src/pages/dashboard/accounting/invoicing/page.jsx
+++ src/pages/dashboard/accounting/invoicing/page.jsx
@@ -3,8 +3,9 @@ import { useSearchParams } from 'react-router';
 import EmptyInvoice from '@/components/dashboard/accounting/invoicing/empty-state';
 import CreateInvoice from '@/components/dashboard/accounting/invoicing/create-invoice';
 import { Button } from '@/components/ui/button';
-import { DownloadIcon, Key, PlusCircle, SettingsIcon } from 'lucide-react';
+import { DownloadIcon, PlusCircle, SettingsIcon } from 'lucide-react';
 import MetricCard from '@/components/dashboard/metric-card';
+import InvoiceTable from '@/components/dashboard/accounting/invoicing/invoice-table';
 
 const invoice = [''];
 const invoiceStats = [
@@ -69,7 +70,7 @@ export default function Invoicing() {
 
   return (
     <div className="my-4 min-h-screen">
-      <div className="flex items-center justify-between">
+      <div className="flex flex-wrap items-center justify-between gap-6">
         <hgroup>
           <h1 className="text-2xl font-bold">Invoice Management</h1>
           <p className="text-sm text-[#7D7D7D]">
@@ -98,18 +99,22 @@ export default function Invoicing() {
       {invoice.length === 0 ? (
         <EmptyInvoice onClick={handleToggleCreateInvoice} />
       ) : (
-        <div className="my-10 flex w-full flex-wrap gap-4">
-          {invoiceStats.length > 0 &&
-            invoiceStats.map((stat, index) => (
-              <MetricCard
-                key={index}
-                className="w-full max-w-[259px]"
-                title={stat.title}
-                unit={stat.unit}
-                emptyState={true}
-                {...stat}
-              />
-            ))}
+        <div>
+          <div className="my-10 flex w-full flex-wrap gap-4">
+            {invoiceStats.length > 0 &&
+              invoiceStats.map((stat, index) => (
+                <MetricCard
+                  key={index}
+                  className="w-full max-w-[259px]"
+                  title={stat.title}
+                  unit={stat.unit}
+                  emptyState={true}
+                  {...stat}
+                />
+              ))}
+          </div>
+
+          <InvoiceTable />
         </div>
       )}
     </div>
