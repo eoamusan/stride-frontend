9a8a9d0e3518e078dcd325988a91b35f087e0c5c
Oluwatobi Amusan <amusantobi@gmail.com>
2025-09-08 14:56:47 +0100

    feat: add trial-balance cta

--- src/pages/dashboard/accounting/bookkeeping/trial-balance/page.jsx
+++ src/pages/dashboard/accounting/bookkeeping/trial-balance/page.jsx
@@ -1,6 +1,15 @@
+import RunReportForm from '@/components/dashboard/accounting/bookkeeping/run-report-form';
+import TrialBalanceCta from '@/components/dashboard/accounting/bookkeeping/trial-balance-cta';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 export default function TrialBalance() {
+  const [reportPeriod, setReportPeriod] = useState('empty');
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
+  const [accountingMethod, setAccountingMethod] = useState('accrual');
+  const [openRunReportForm, setOpenRunReportForm] = useState(false);
+
   return (
     <div className="my-4 min-h-screen">
       <div className="flex flex-wrap items-center justify-between gap-6">
@@ -18,6 +27,26 @@ export default function TrialBalance() {
           <Button className={'h-10 rounded-2xl text-sm'}>Save</Button>
         </div>
       </div>
+      <div className="mt-10">
+        <TrialBalanceCta
+          reportPeriod={reportPeriod}
+          onReportPeriodChange={setReportPeriod}
+          fromDate={fromDate}
+          onFromDateChange={setFromDate}
+          toDate={toDate}
+          onToDateChange={setToDate}
+          accountingMethod={accountingMethod}
+          onAccountingMethodChange={setAccountingMethod}
+          onRunReport={() => setOpenRunReportForm(true)}
+          onFilter={() => {}}
+        />
+      </div>
+
+      <RunReportForm
+        isOpen={openRunReportForm}
+        onClose={() => setOpenRunReportForm(false)}
+        onSubmit={() => {}}
+      />
     </div>
   );
 }
