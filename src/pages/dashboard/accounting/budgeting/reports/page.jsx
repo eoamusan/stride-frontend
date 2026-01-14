852befcd8ca801db6d1a41d4a1192fe782598fdc
ChristianArk <onyenekechristian@gmail.com>
2026-01-14 22:01:50 +0100

    feat: add ReportSkeleton component and integrate mock loading state in BudgetingAnalytics page

--- src/pages/dashboard/accounting/budgeting/reports/page.jsx
+++ src/pages/dashboard/accounting/budgeting/reports/page.jsx
@@ -10,12 +10,14 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { ChartArea, ChartColumn } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ColorPicker from '@/components/ui/color-picker';
 import AreaMetricCard from '@/components/dashboard/area-metric-card';
 import BarChartOverview from '@/components/dashboard/bar-metric-card';
+import ReportSkeleton from '@/components/dashboard/accounting/budgeting/reports/reports-skeleton';
 
 export default function BudgetingAnalytics() {
+  const [isLoading, setIsLoading] = useState(true)
   const [chartType, setChartType] = useState('areaChart')
   const [period, setPeriod] = useState('yearly')
   const [metric, setMetric] = useState('budgetVsActual')
@@ -43,90 +45,99 @@ const sampleChartConfig = {
   },
 };
 
+useEffect(() => {
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 3000)
+}, [])
+
   return (
     <div className='my-4 min-h-screen'>
       <>
         <BudgetHeader />
-        <div className="relative mt-10 bg-white p-6 rounded-2xl">
-          <h3 className='mb-4 font-medium text-xl'>Variance Alerts</h3>
-          <div className='grid grid-cols-1 lg:grid-cols-3 gap-2'>
-            <VarianceCard title="Marketing Budget Overrun" description="6.25% over budget ($50K excess)" className="bg-red-100  text-red-900" />
-            <VarianceCard title="HR Budget On Track" description="2.22% under budget, excellent performance" className="bg-green-100  text-green-900"  />
-            <VarianceCard title="Sales Budget Warning" description="Approaching 90% of budget limit" className="bg-orange-100  text-orange-900"  />
+        { isLoading && <div className='mt-10'><ReportSkeleton /></div> }
+        { !isLoading && <>
+          <div className="relative mt-10 bg-white p-6 rounded-2xl">
+            <h3 className='mb-4 font-medium text-xl'>Variance Alerts</h3>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-2'>
+              <VarianceCard title="Marketing Budget Overrun" description="6.25% over budget ($50K excess)" className="bg-red-100  text-red-900" />
+              <VarianceCard title="HR Budget On Track" description="2.22% under budget, excellent performance" className="bg-green-100  text-green-900"  />
+              <VarianceCard title="Sales Budget Warning" description="Approaching 90% of budget limit" className="bg-orange-100  text-orange-900"  />
+            </div>
           </div>
-        </div>
-        { chartType === 'areaChart' && <AreaMetricCard
-          className={'h-full w-full mt-10'}
-          title={'Budget Vs-Actual - Bar Chart'}
-          description={'Monthly comparison over the past year'}
-          chartData={sampleChartData}
-          chartConfig={sampleChartConfig}
-        /> }
-        { chartType === 'barChart' && <BarChartOverview
-            title="Budget Vs-Actual - Bar Chart"
-            description={'Monthly comparison over the past year'}
-            chartConfig={sampleChartConfig}
-            chartData={sampleChartData}
-            numberOfBars={2}
-            showLegend={true}
+          { chartType === 'areaChart' && <AreaMetricCard
             className={'h-full w-full mt-10'}
+            title={'Budget Vs-Actual - Bar Chart'}
+            description={'Monthly comparison over the past year'}
+            chartData={sampleChartData}
+            chartConfig={sampleChartConfig}
           /> }
-        <div className="relative mt-10 bg-white p-6 rounded-2xl">
-          <h3 className='mb-4 font-medium text-xl'>Budget Analytics & Visualization</h3>
-          <div className='grid grid-cols-1 lg:grid-cols-4 gap-8'>
-            <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
-              <span>Chart Type</span>
-              <Select defaultValue={chartType} onValueChange={setChartType}>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a chart type" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="barChart"><ChartColumn /> Bar Chart</SelectItem>
-                    <SelectItem value="areaChart"><ChartArea /> Area Chart</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
-              <span>Period</span>
-              <Select defaultValue={period}>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a period" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Period</SelectLabel>
-                    <SelectItem value="yearly">Yearly</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
-              <span>Metric</span>
-              <Select defaultValue={metric}>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a period" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="budgetVsActual">Budget vs Actual</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
-              <span>Color scheme</span>
-              <ColorPicker
-                showTitle={false}
-                selectedColor={selectedColor}
-                onColorChange={(color) => {
-                  setSelectedColor(color);
-                }}
-              />
+          { chartType === 'barChart' && <BarChartOverview
+              title="Budget Vs-Actual - Bar Chart"
+              description={'Monthly comparison over the past year'}
+              chartConfig={sampleChartConfig}
+              chartData={sampleChartData}
+              numberOfBars={2}
+              showLegend={true}
+              className={'h-full w-full mt-10'}
+            /> }
+          <div className="relative mt-10 bg-white p-6 rounded-2xl">
+            <h3 className='mb-4 font-medium text-xl'>Budget Analytics & Visualization</h3>
+            <div className='grid grid-cols-1 lg:grid-cols-4 gap-8'>
+              <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
+                <span>Chart Type</span>
+                <Select defaultValue={chartType} onValueChange={setChartType}>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a chart type" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="barChart"><ChartColumn /> Bar Chart</SelectItem>
+                      <SelectItem value="areaChart"><ChartArea /> Area Chart</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
+                <span>Period</span>
+                <Select defaultValue={period}  onValueChange={setPeriod}>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a period" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Period</SelectLabel>
+                      <SelectItem value="yearly">Yearly</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
+                <span>Metric</span>
+                <Select defaultValue={metric} onValueChange={setMetric}>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a period" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="budgetVsActual">Budget vs Actual</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className='w-full flex flex-col gap-2 [&>span]:font-medium'>
+                <span>Color scheme</span>
+                <ColorPicker
+                  showTitle={false}
+                  selectedColor={selectedColor}
+                  onColorChange={(color) => {
+                    setSelectedColor(color);
+                  }}
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </> }
       </>
     </div>
   );
