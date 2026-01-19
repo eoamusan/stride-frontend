ab7bec04e2537c12ffc61a1b595776add2ee1d70
Adewale Stephen <adewalestephennifemi@gmail.com>
2026-01-19 20:12:20 +0100

    reused already existing components

--- src/pages/dashboard/hr/overview/page.jsx
+++ src/pages/dashboard/hr/overview/page.jsx
@@ -1,11 +1,45 @@
+import MetricCard from '@/components/dashboard/metric-card';
+
 export default function Overview() {
+  const metricsData = [
+    {
+      title: 'Number of Staffs',
+      value: 30,
+      percentage: 3,
+    },
+    {
+      title: 'Turnover Rate',
+      value: 50,
+      unit: '%',
+      percentage: -2,
+      isPositive: false,
+    },
+    {
+      title: 'Employee Sentiment',
+      value: 70,
+      unit: '%',
+      percentage: 5,
+    },
+    {
+      title: 'Attendance Rate',
+      value: 50,
+      unit: '%',
+      percentage: 2,
+    },
+  ];
+
   return (
-    <div className="my-4 min-h-screen">
-      <div className="flex flex-wrap items-center justify-between gap-6">
-        <hgroup>
-          <h1 className="text-2xl font-bold">Onboarding</h1>
-          <p className="text-sm text-[#7D7D7D]">Manage employee onboarding</p>
-        </hgroup>
+    <div className="my-10">
+      <hgroup>
+        <h1 className="text-2xl font-bold">Account Overview</h1>
+        <p className="text-sm text-[#7D7D7D]">
+          👋 Welcome back! Here's what's happening with your business.
+        </p>
+      </hgroup>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        {metricsData.map((metric) => (
+          <MetricCard key={metric.title} {...metric} emptyState={true} />
+        ))}
       </div>
     </div>
   );
