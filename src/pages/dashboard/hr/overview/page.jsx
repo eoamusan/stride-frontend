b4cfb4e3d487f888198866692c3bbef2908ddc9c
Adewale Stephen <adewalestephennifemi@gmail.com>
2026-01-19 04:13:53 +0100

    add(hr): created hr dir and other dir and file for hr layout setup

---
+++ src/pages/dashboard/hr/overview/page.jsx
@@ -0,0 +1,12 @@
+export default function Overview() {
+  return (
+    <div className="my-4 min-h-screen">
+      <div className="flex flex-wrap items-center justify-between gap-6">
+        <hgroup>
+          <h1 className="text-2xl font-bold">Onboarding</h1>
+          <p className="text-sm text-[#7D7D7D]">Manage employee onboarding</p>
+        </hgroup>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/dashboard/hr/perfomance/page.jsx
@@ -0,0 +1,14 @@
+export default function Performance() {
+  return (
+    <div className="my-4 min-h-screen">
+      <div className="flex flex-wrap items-center justify-between gap-6">
+        <hgroup>
+          <h1 className="text-2xl font-bold">Performance</h1>
+          <p className="text-sm text-[#7D7D7D]">
+            Track and manage employee performance
+          </p>
+        </hgroup>
+      </div>
+    </div>
+  );
+}
